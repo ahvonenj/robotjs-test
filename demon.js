a138dc7dd2ba9b5bf7676e519eeb3f1653194443
Jonah <jonah.ahvonen@gmail.com>
2017-04-05 16:51:10 +0300

    Initial HID

--- demon.js
+++ demon.js
@@ -1,2 +1,23 @@
+// https://github.com/node-hid/node-hid
+// npm install -g node-gyp
+// npm install -g node-pre-gyp
+// Visual C++ Compiler and Python 2.7
+
+var keypress = require('keypress');
+
+// Log daemon pid
 console.log('DAEMON RUNNING, PID: ' + process.pid);
-//while(true){}
+
+
+// Begin emitting
+keypress(process.stdin);
+
+process.stdin.on('keypress', function(ch, key)
+{
+	console.log('Key pressed', ch, key)
+});
+
+//rocess.stdin.setRawMode(true);
+//process.stdin.resume();
+
+
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-var daemon = require('daemon');
+var child_process = require('child_process');
 
 // Log parent pid
 console.log('INDEX RUNNING, PID: ' + process.pid);
 
 
 // Spawn child process
-var child = daemon.daemon('demon.js', null,
+var child = child_process.spawn(process.execPath, ['demon.js'], 
 {
-	stdout: 'pipe'
+	stdio: 'pipe',
+	env: process.env,
+	cwd: process.cwd,
+	detached: true
 });
+
+child.unref();
 
 // Log child process stdout-output
 child.stdout.on('data', function(data)
@@ -16,4 +21,8 @@ child.stdout.on('data', function(data)
 	console.log(data.toString());
 });
 
-
+// Log child process stderr-output
+child.stderr.on('data', function(data)
+{
+	console.log(data.toString());
+});
